1ce28be6bd9fb5d94f5db40ca72b46b57f46095e
Shohag <ruponstat1203@gmail.com>
2025-02-24 11:14:36 +0600

    Solution of 5 problems using if-else condition added

---
+++ geeksforgeeks/3(a). If-else (Decision Making).c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    int i = 20;
+
+    if(i==15){
+        printf("i is 10");
+    }
+    else if(i==20){
+        printf("i is 15");
+    }
+    else{
+        printf("i is not present");
+}
+    return 0;
+}
---
+++ geeksforgeeks/3(b). Switch case.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(){
+    int var = 2;
+
+    switch(var){
+case 1:
+    printf("case 1 is executed.");
+    break;
+case 2:
+    printf("case 2 is exeuted");
+    break;
+case 3:
+    printf("case 3 is exeuted");
+    break;
+case 4:
+    printf("case 4 is exeuted");
+    break;
+default:
+    printf("case is not executed");
+    break;
+    }
+}
